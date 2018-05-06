425938420f2483183b48cf94e87303a6d9e4d157
Michael Ballard <ballard.michael9@gmail.com>
2018-05-06 10:24:26 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 def speak_to_grandma(string)
   saying = string
   if string.uppercase == string
-    "No, NOT SINCE 1938!" 
+    "NO, NOT SINCE 1938!" 
   elsif string == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   else 
